4c51f5354e18c5a18e0979138bab0fa8555b013a
iishimwe <ixacson@gmail.com>
2021-12-15 06:51:24 +0200

    Setting up firebase settings

---
+++ js/firebase.js
@@ -0,0 +1,42 @@
+// HOLDS FIREBASE CONFIG
+let appFirebase = {};
+
+
+(() => {
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyCnxxUq_j9Sblj3YbwciKdkq5lH417s7kg",
+        authDomain: "my-brand-53555.firebaseapp.com",
+        projectId: "my-brand-53555",
+        storageBucket: "my-brand-53555.appspot.com",
+        messagingSenderId: "450269858536",
+        appId: "1:450269858536:web:7bd50cf0d9c59e1d1a175d",
+        measurementId: "G-JPL67SH209"
+    };
+
+    // INITIALIZE MY-BRAND APP PROJECT
+    firebase.initializeApp(firebaseConfig);
+    // TO BE ABLE TO USE 
+    appFirebase = firebase;
+
+    // CREATE
+    const createBlog = () => {
+
+    };
+
+    // READ
+    const readBlog = () => {
+
+    };
+
+    // UPDATE
+    const updateBlog = () => {
+
+    };
+
+    // DELETE
+    const deleteBlog = () => {
+
+    };
+
+})();
--- js/main.js
+++ js/main.js
@@ -4,3 +4,46 @@ collapsibles.forEach((item) =>
         this.classList.toggle("collapsible--expanded");
     })
 );
+
+// FIREBASE STUFFS
+let mainFunc = {};
+
+(() => {
+
+    // AUTHENTICATION
+    appFirebase.auth().createUserWithEmailAndPassword(email = 'fils@gmail.com', password = '_fils123')
+        .then((user) => {
+            // Signed in 
+            // var user = userCredential.user;
+            // ...
+            console.log(user);
+        })
+        .catch((error) => {
+            // let errorCode = error.code;
+            // let errorMessage = error.message;
+            // console.log(errorCode);
+            // console.log(errorMessage);
+            console.log(error);
+        });
+
+    // CREATE
+    const createBlog = () => {
+
+    };
+
+    // READ
+    const readBlog = () => {
+
+    };
+
+    // UPDATE
+    const updateBlog = () => {
+
+    };
+
+    // DELETE
+    const deleteBlog = () => {
+
+    };
+
+})();
