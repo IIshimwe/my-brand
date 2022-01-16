9001feea4d127c06c468e5db20ca354dbc95e883
iishimwe <ixacson@gmail.com>
2022-01-16 13:16:59 +0200

    Creation of user schema as well as propeties validations

---
+++ backend/models/user.js
@@ -0,0 +1,34 @@
+import Joi from 'joi';
+import mongoose from 'mongoose';
+
+export const Genre = mongoose.model('Genre', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        maxlength: 1024
+    }
+}));
+
+export function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    };
+
+    return Joi.validate(user, schema);
+}
