cc0345988fc0ad5d36e90773d6b7495b5f815e05
iishimwe <ixacson@gmail.com>
2022-01-18 17:31:44 +0200

    Add article model

---
+++ backend/src/models/article.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        maxlength: 255
+    },
+    // img:
+    // {
+    //     data: Buffer,
+    //     contentType: String
+    // },
+    author: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, { timestamps: true });
+
+const Article = mongoose.model('Article', articleSchema);
+
+export default Article;
