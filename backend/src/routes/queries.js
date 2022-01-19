d5a477e35109488aa12766fb5360b4c0e516fc8a
iishimwe <ixacson@gmail.com>
2022-01-19 16:38:34 +0200

    Create a GET method to the user queries and assign authorization to it.

--- backend/src/routes/queries.js
+++ backend/src/routes/queries.js
@@ -1,6 +1,7 @@
 import { pick } from 'lodash';
 import { Querie, validate } from '../models/query';
 import express from 'express';
+import autho from '../middlewares/autho';
 const router = express.Router();
 
 router.post('/', async (req, res) => {
@@ -11,6 +12,11 @@ router.post('/', async (req, res) => {
     await query.save();
 
     res.send('Message has been sent');
+});
+
+router.get('/', autho, async (req, res) => {
+    const queries = await Querie.find();
+    res.send(queries);
 });
 
 export default router;
