3f701e45d0d3a1928ef409de122c4ec7308f0ee3
iishimwe <ixacson@gmail.com>
2022-01-26 12:22:02 +0200

    Deleted all the backend files and store it on its own repo

--- backend/src/startup/db.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'mongoose';
-
-module.exports = function () {
-    connect('mongodb://localhost/capstone')
-        .then(() => console.log('Connected to mongoDB'))
-        .catch(err => console.error('Could not connect to MongoDB...', err));
-
-};
